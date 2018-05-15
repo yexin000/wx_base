a646f9f7b7467a672e3c38da600f55e0b58105d5
yexin000 <240973751@qq.com>
2018-05-15 23:10:48 +0800

    集成redis，小程序授权登陆获取用户信息

--- src/main/java/cn/trustway/weixin/bean/WeixinUser.java
+++ src/main/java/cn/trustway/weixin/bean/WeixinUser.java
@@ -6,33 +6,100 @@ package cn.trustway.weixin.bean;
  *
  */
 public class WeixinUser extends BaseBean {
-	private Integer xh;// 序号
-	private String wxid;// 微信openid
-	private String zt;// 状态
-	private String ylzd1;
-	
-	public Integer getXh() {
-		return xh;
+	/**
+	 * 主键
+	 */
+	private Integer id;
+	/**
+	 * 微信id
+	 */
+	private String wxid;
+	/**
+	 * 手机号码
+	 */
+	private String phoneNum;
+	/**
+	 * 余额
+	 */
+	private Double balance;
+	/**
+	 * 提取中金额
+	 */
+	private Double moneyExtracting;
+	/**
+	 * 已提取金额
+	 */
+	private Double extractedMoney;
+	/**
+	 * 昵称
+	 */
+	private String nickName;
+	/**
+	 * 微信头像地址
+	 */
+	private String avatarUrl;
+
+	public Integer getId() {
+		return id;
 	}
-	public void setXh(Integer xh) {
-		this.xh = xh;
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
+
 	public String getWxid() {
 		return wxid;
 	}
+
 	public void setWxid(String wxid) {
 		this.wxid = wxid;
 	}
-	public String getZt() {
-		return zt;
+
+	public String getPhoneNum() {
+		return phoneNum;
 	}
-	public void setZt(String zt) {
-		this.zt = zt;
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
 	}
-	public String getYlzd1() {
-		return ylzd1;
+
+	public Double getBalance() {
+		return balance;
 	}
-	public void setYlzd1(String ylzd1) {
-		this.ylzd1 = ylzd1;
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getMoneyExtracting() {
+		return moneyExtracting;
+	}
+
+	public void setMoneyExtracting(Double moneyExtracting) {
+		this.moneyExtracting = moneyExtracting;
+	}
+
+	public Double getExtractedMoney() {
+		return extractedMoney;
+	}
+
+	public void setExtractedMoney(Double extractedMoney) {
+		this.extractedMoney = extractedMoney;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
 	}
 }
