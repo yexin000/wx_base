3b2da8ca6026f33c683833c055f61db34fca0e28
yexin000 <240973751@qq.com>
2018-09-29 15:05:00 +0800

    我的鉴定

--- src/main/java/cn/trustway/weixin/controller/IdentifyController.java
+++ src/main/java/cn/trustway/weixin/controller/IdentifyController.java
@@ -4,12 +4,17 @@ package cn.trustway.weixin.controller;
 import cn.trustway.weixin.bean.Identify;
 import cn.trustway.weixin.model.IdentifyModel;
 import cn.trustway.weixin.service.IdentifyService;
+import cn.trustway.weixin.util.HtmlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,5 +39,27 @@ public class IdentifyController extends BaseController {
     public ModelAndView list(IdentifyModel model, HttpServletRequest request) throws Exception {
         Map<String, Object> context = getRootMap();
         return forword("auction/identifyList", context);
+    }
+
+    /**
+     * json 列表页面
+     *
+     * @param model
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/dataList", method = RequestMethod.POST)
+    public void dataList(IdentifyModel model, HttpServletResponse response) throws Exception {
+        queryDataList(model, response);
+    }
+
+    private void queryDataList(IdentifyModel model, HttpServletResponse response) throws Exception {
+        List<Identify> dataList = identifyService.queryByList(model);
+        // 设置页面数据
+        Map<String, Object> jsonMap = new HashMap<String, Object>();
+        jsonMap.put("total", identifyService.queryByCount(model));
+        jsonMap.put("rows", dataList);
+        HtmlUtil.writerJson(response, jsonMap);
     }
 }
